09dab9652e0513d5e2b57ac194ab94e0adeac4be
shall053 <shall053@odu.edu>
2026-07-08 18:12:12 -0400

    Update abilities.ml

--- src/abilities.ml
+++ src/abilities.ml
@@ -1,15 +1,31 @@
 
-(*Define outcome of the ability*)
-type effect = 
-    | Damage of float
-    | StatusEffect of string * int
-    | Overclock of float (*what it does to the caster*)
+(*define the ability*)
+type effect =
+    | Damage_Qi of float
+    | Drain_Qi of float
+    | Buff_Regen of float
+    | Grid_Lock of int 
 
-(*Each technique has pre-requisites*)
-type technique ={
+(*technique*)
+type ability ={
     name : string;
-    cost : float; (*Qi cost*)
-    execution_logic : unit -> effect; (*what to run*)
+    qi_cost : float;
+    effects: effect list;
 }
 
-(*validate against pool then execute*)
+
+(*dispatch, apply logic to entity and environment*)
+let execute_ability (abil: ability) (user_qi: float) (target: meridian_state) : (float * meridian_state) option =
+  if user_qi < abil.qi_cost then None 
+  else
+    let new_user_qi = user_qi -. abil.qi_cost in
+    let apply_effect state eff =
+      match eff with
+      | Damage_Qi amount -> { state with current_qi = Float.max 0.0 (state.current_qi -. amount) }
+      | Drain_Qi amount  -> { state with current_qi = Float.max 0.0 (state.current_qi -. amount) }
+      | Buff_Regen amt   -> { state with internal_base_regen = state.internal_base_regen +. amt }
+      (* Refinement: Added lock_duration to track the cooldown *)
+      | Grid_Lock duration -> { state with is_blocked = true; lock_duration = duration }
+    in
+    let updated_target = List.fold_left apply_effect target abil.effects in
+    Some (new_user_qi, updated_target)
